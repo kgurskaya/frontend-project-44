8d072a112e45e6d54d757177d6b91e4268311abc
Ksenia Gurskaya <ksenia@gurskaya.com>
2022-11-21 22:45:53 +0300

    added brain progression

---
+++ src/games/progression.js
@@ -0,0 +1,43 @@
+import {
+  greeting, round, getRandomNumber, getRandomNumberBetween,
+} from '../index.js';
+
+const getProgression = () => {
+  const start = getRandomNumber(100);
+  const randomIncrement = getRandomNumber(10);
+  const result = [start];
+  for (let i = 0; i < getRandomNumberBetween(5, 10); i += 1) {
+    result.push(result[result.length - 1] + randomIncrement);
+  }
+  const randomIndex = getRandomNumberBetween(0, result.length);
+  const correctAnswer = result[randomIndex];
+  result[randomIndex] = '..';
+  return {
+    correctAnswer,
+    pregression: result.join(' '),
+  };
+};
+
+const playGame = () => {
+  const name = greeting('What number is missing in the progression?');
+  let score = 0;
+  while (score < 3) {
+    const progression = getProgression();
+    const userAnswer = Number(round(`${progression.pregression}`));
+    const result = progression.correctAnswer === userAnswer;
+    if (result === true) {
+      score += 1;
+      if (score === 3) {
+        console.log(`Congratulations, ${name}!`);
+      } else {
+        console.log('Correct!');
+      }
+    } else {
+      console.log(`'${userAnswer}' is wrong answer ;(. Correct answer was '${progression.correctAnswer}'.`);
+      console.log(`Let's try again, ${name}`);
+      break;
+    }
+  }
+};
+
+export default playGame;
--- src/index.js
+++ src/index.js
@@ -1,6 +1,11 @@
 import readlineSync from 'readline-sync';
 
-export const getRandomNumber = (maxNumber) => Math.floor(Math.random() * maxNumber);
+export const getRandomNumber = (max) => Math.floor(Math.random() * max);
+export const getRandomNumberBetween = (min, max) => {
+  const minimum = Math.ceil(min);
+  const maximum = Math.floor(max);
+  return Math.floor(Math.random() * (maximum - minimum) + minimum);
+};
 
 export const askName = () => {
   const name = readlineSync.question('May I have your name? ');
