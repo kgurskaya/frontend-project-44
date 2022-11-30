a57b4edc7bc142fe0afb22601034456688ad4265
Ksenia Gurskaya <ksenia@gurskaya.com>
2022-11-30 22:36:36 +0300

    refactoring brain prime

--- src/games/prime.js
+++ src/games/prime.js
@@ -3,10 +3,15 @@ import { getRandomNumber } from '../helpers.js';
 
 const gameDescription = 'Answer "yes" if given number is prime. Otherwise answer "no".';
 
-// eslint-disable-next-line max-len
-const prime = [2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97];
-
-const isPrime = (number) => prime.includes(number);
+const isPrime = (number) => {
+  if (number === 1) return false;
+  for (let i = 2; i < Math.sqrt(number); i += 1) {
+    if (number % i === 0) {
+      return false;
+    }
+  }
+  return true;
+};
 
 const getGameData = () => {
   const question = getRandomNumber(100);
